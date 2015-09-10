5cf629dfcee73d3aa4aeb06d41c5392df22b8ff1
Hack Reactor Students <accounts@hackreactor.com>
2015-09-09 20:48:35 -0700

    Serve index.html from server

--- web/request-handler.js
+++ web/request-handler.js
@@ -7,21 +7,31 @@ var fs = require('fs')
 var sendResponse = function(res, data, statusCode){
   statusCode = statusCode || 200;
   res.writeHead(statusCode, http_helpers.headers);
-  res.end(JSON.stringify(data));
+  res.end(data);
 };
 
-// var responses = {
-//   "GET": function(req, res) {
-//     sendResponse(res, req.url);
-//   },
-//   "POST": function(req, res) {
-//     sendResponse(res, data?, 201);
-//   },
-//   "OPTIONS": function(req, res) {
-//     sendResponse(res, );
-//   }
-// };
+var responses = {
+  "GET": function(req, res) {
+    // use fs to read the HTML file
+    var file = path.join(__dirname, './public/index.html');
+    console.log(file);
+    fs.readFile(file, 'utf8', function(err, data) {
+      sendResponse(res, data);
+    })
+    // pass that as data to sendResponse
+    
+    //Then send response
+    // sendResponse(res, data?);
+  },
+  "POST": function(req, res) {
+
+    sendResponse(res, "yessir!", 201);
+  },
+  "OPTIONS": function(req, res) {
+    sendResponse(res);
+  }
+};
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  responses.GET(req, res);
 };
